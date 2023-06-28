3c4bebf5ed5628d57b89b9e8de7ef3951735942f
PeterJanasz <petertjanasz@gmail.com>
2023-06-28 16:29:51 -0700

    added if statements to validate user input and criteria

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ var generateBtn = document.querySelector("#generate");
 //password criteria
 function generatePassword(){
   
-  var length = prompt ("Choose a lenghth between 8-128 characters long.");
-  var criteria = confirm ("Must choose one of the following criteria.");
-  var upperCase = confirm ("Would you like to use upper case letters?");
-  var lowerCase = confirm ("Would you like to use lowercase letters");
-  var numeric = confirm ("Would you like to use numeric characters?");
-  var special = confirm ("Would you like to use special characters?");
+  var length = window.prompt ("Choose a lenghth between 8-128 characters long.");
+  console.log(length)
+  var criteria = window.alert ("Must choose one of the following criteria.");
+  console.log(criteria)
+  var upperCase = window.confirm ("Would you like to use upper case letters?");
+  console.log(upperCase)
+  var lowerCase = window.confirm ("Would you like to use lowercase letters");
+  console.log(lowerCase)
+  var numeric = window.confirm ("Would you like to use numeric characters?");
+  console.log(numeric)
+  var special = window.confirm ("Would you like to use special characters?");
+  console.log(special)
+
+  //Validate user input logic
+  if (length < 8 || length > 128) {
+    alert ("Password length must be between 8 and 128 characters long.");
+    return;
+  }
+
+  if (!upperCase && !lowerCase && !numeric && !special){
+    alert("Must choose at least one character type!")
+  }
 }
 
-//validate criteria
 
-if (length < 8 || length > 128) {
-  alert ("Password length must be between 8 and 128 characters long.");
-  return;
-}
+
+
+
+
 
 
 // Write password to the #password input
