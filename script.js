d420e7f0b31ec1ef09a86a137dc15d0fb4da78bb
PeterJanasz <petertjanasz@gmail.com>
2023-06-27 17:20:07 -0700

    password criteria prompts

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//password criteria
+function generatePassword(){
+  var length = prompt("Choose a lenghth between 8-128 characters long.");
+  var upperCase = prompt("Would you like to use upper case letters?");
+  var lowerCase = prompt("Would you like to use lowercase letters");
+  var numeric = prompt ("Would you like to use numeric characters?");
+  var special = prompt ("Would you like to use special characters?");
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
